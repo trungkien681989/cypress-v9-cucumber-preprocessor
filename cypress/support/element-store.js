87f8d4f90d9617002b85818f40f3b3bee5e69037
Kien <Kien.TrungBui@coxautoinc.com>
2022-07-20 20:37:06 +0700

    Add element locators store

---
+++ cypress/support/element-store.js
@@ -0,0 +1,18 @@
+/* LOCATORS
+ Should place them in order: left-to-right & top-to-bottom */
+
+/* HEADER
+========================================================================= */
+export const backToHomePage = 'button[aria-label="Back to homepage"]';
+
+/* ALL PRODUCTS
+========================================================================= */
+export const itemsPerPage = 'div[class*="paginator-page-size-label"]';
+
+export const elementStore = {
+  // HEADER
+  'Back To Home Page': backToHomePage,
+
+  // ALL PRODUCTS
+  'Items Per Page': itemsPerPage,
+};
